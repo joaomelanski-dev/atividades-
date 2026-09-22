ee0a6f8fbf88f24d6029cdbfeab2f3130c2cef02
joaomelanski-dev <joao.melanski@escola.pr.gov.br>
2026-09-22 08:46:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-function parouimpar(){
-    let numero;
-    let resultado;
+function maior(){
+    let maior;
+    let menor;
 
-    numero = Number(prompt("Digite o número:"));
+    let resultado = 
+    maior = Number(prompt("Digite o número"))
+    if()
 
-    if(numero % 2 == 0){
-        alert("O número é par");
-
-    }else{
-        alert("O número é ímpar");
-    }
 }
+
